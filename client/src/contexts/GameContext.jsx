71da424c40a0f59dea26bdbedfc1e1c30c645504
arnav2000agr <92395924+arnav2000agr@users.noreply.github.com>
2024-03-15 23:12:25 +0530

    bugs and fixes

--- client/src/contexts/GameContext.jsx
+++ client/src/contexts/GameContext.jsx
@@ -2,7 +2,8 @@ import { createContext, useState } from "react";
 
 import { io } from "socket.io-client";
 
-const socket = io.connect("http://localhost:8000");
+// const socket = io.connect("http://localhost:8000");
+const socket = io.connect("https://arnav-scribble.onrender.com");
 
 export const GameContext = createContext({
   username: "",
